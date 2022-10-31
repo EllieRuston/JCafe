cfe3fd0a1020843b0ca16af27e42cb1fc9f66deb
EllieRuston <88114202+EllieRuston@users.noreply.github.com>
2022-10-31 19:52:08 +1300

    competed menu pages and social media links

--- JCafe/Views/AboutPage.xaml.cs
+++ JCafe/Views/AboutPage.xaml.cs
@@ -1,9 +1,40 @@
 namespace JCafe.Views;
 
+using Android.OS;
+using System.Diagnostics;
+using Xamarin.Google.Crypto.Tink.Proto;
+
 public partial class AboutPage : ContentPage
 {
 	public AboutPage()
 	{
 		InitializeComponent();
+	}
+
+	private async void FB_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			Uri uri = new Uri("https://www.facebook.com/FamilyGuy/about");
+			await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+		}
+		catch (Exception ex)
+		{
+			// error message
+		}
+	}
+
+	private async void Instagram_Clicked(object sender, EventArgs e)
+	{
+        try
+        {
+            Uri uri = new Uri("https://www.instagram.com/itsdougthepug");
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+        catch (Exception ex)
+        {
+            // error message
+        }
+       
 	}
 }
